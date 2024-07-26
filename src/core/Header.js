94451e7412324e94218350df3d041bdc43d85ebd
M Veeramakali Vignesh <veeramakali1@gmail.com>
2024-07-25 20:31:18 -0700

    fixed navbar bug

--- src/core/Header.js
+++ src/core/Header.js
@@ -6,10 +6,10 @@ export default function Header() {
             <nav className="navbar navbar-expand-md navbar-dark">
                 <a className="navbar-brand">&nbsp; Games AI</a>
                 <div className="navbar-nav">
-                    <Link to="/">
+                    <Link to="/about">
                         <a className="nav-item nav-link">About</a>
                     </Link>
-                    <Link to="/cannon">
+                    <Link to="/">
                         <a className="nav-item nav-link">Cannon</a>
                     </Link>
                     <a className="nav-item nav-link" href="https://github.com/cant12/games-ai-UI">Github</a>
